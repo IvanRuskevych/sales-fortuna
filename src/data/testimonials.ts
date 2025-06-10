aee7e714055275192151900ddc60b036b2615576
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-06-10 13:17:36 +0300

    Create components Button, Card, Title

---
+++ src/data/testimonials.ts
@@ -0,0 +1,52 @@
+import type {Testimonial} from "../types";
+
+export const testimonials: Testimonial[] = [
+    {
+        company: "Serene Living Products",
+        logo: "serene-logo.jpg",
+        text: "Sales Fortuna made managing sales easier and helped us focus on customers. Its tools have been crucial for our growth and client satisfaction.",
+        name: "Ethan Morgan",
+        title: "Founder and CEO, Serene Living Products",
+        avatar: "ethan_morgan.jpg",
+    },
+    {
+        company: "Starlight Creations",
+        logo: "starlight-logo.jpg",
+        text: "Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.",
+        name: "Olivia Hayes",
+        title: "Owner, Starlight Creations",
+        avatar: "olivia_hayes.jpg",
+    },
+    {
+        company: "Opulent Living Group",
+        logo: "opulent-logo.jpg",
+        text: "Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.",
+        name: "Alexander Reed",
+        title: "Co-Founder, Opulent Living Group",
+        avatar: "alexander_reed.jpg",
+    },
+    {
+        company: "Opulent Living Group",
+        logo: "opulent-logo.jpg",
+        text: "Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.",
+        name: "Alexander Reed",
+        title: "Co-Founder, Opulent Living Group",
+        avatar: "alexander_reed.jpg",
+    },
+    {
+        company: "Starlight Creations",
+        logo: "starlight-logo.jpg",
+        text: "Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.",
+        name: "Olivia Hayes",
+        title: "Owner, Starlight Creations",
+        avatar: "olivia_hayes.jpg",
+    },
+    {
+        company: "Serene Living Products",
+        logo: "serene-logo.jpg",
+        text: "Sales Fortuna made managing sales easier and helped us focus on customers. Its tools have been crucial for our growth and client satisfaction.",
+        name: "Ethan Morgan",
+        title: "Founder and CEO, Serene Living Products",
+        avatar: "ethan_morgan.jpg",
+    },
+];
